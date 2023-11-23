af531bee403a44bfe5f302d832e98c2a10272081
wahyuWs <wahyusantozo36@gmail.com>
2023-11-23 17:48:09 +0700

    description section

--- app/src/main/java/com/home/moviesappjc/ui/navigation/Detail.kt
+++ app/src/main/java/com/home/moviesappjc/ui/navigation/Detail.kt
@@ -3,6 +3,7 @@ package com.home.moviesappjc.ui.navigation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
@@ -22,6 +24,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -58,6 +64,11 @@ fun PreviewDetail() {
     ) {
         MediaPlayerSection()
         TitleAndDetailSection(fontFamily = lattoFamily)
+        ReleaseDateAndGenreSection(fontFamily = lattoFamily)
+        DescriptionSection(
+            fontFamily = lattoFamily,
+            description = "Rey (Daisy Ridley) finally manages to find the legendary Jedi knight, Luke Skywalker (Mark Hamill) on an island with a magical aura. The heroes of The Force Awakens including Leia, Finn. \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\""
+        )
     }
 }
 
@@ -168,4 +179,136 @@ fun TitleAndDetailSection(fontFamily: FontFamily) {
         thickness = 1.dp,
         modifier = Modifier.padding(horizontal = 24.dp)
     )
+}
+
+@Composable
+fun ReleaseDateAndGenreSection(fontFamily: FontFamily) {
+    Row(
+        modifier = Modifier
+            .padding(start = 24.dp, top = 16.dp, end = 24.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Release date",
+            fontFamily = fontFamily,
+            fontSize = 16.sp,
+            color = Color.White,
+            modifier = Modifier
+                .padding(end = 56.dp)
+        )
+        Text(
+            text = "Genre",
+            fontFamily = fontFamily,
+            fontSize = 16.sp,
+            color = Color.White
+        )
+    }
+    Row(
+        modifier = Modifier
+            .padding(start = 24.dp, top = 10.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "December 9, 2017",
+            fontFamily = fontFamily,
+            fontSize = 12.sp,
+            color = TextColorDetail,
+            modifier = Modifier
+                .padding(end = 45.dp)
+        )
+        Box(
+            modifier = Modifier
+                .padding(end = 12.dp)
+                .border(1.dp, Color.DarkGray, RoundedCornerShape(4.dp))
+                .clip(RoundedCornerShape(4.dp))
+                .wrapContentSize()
+                .background(BoxBackground)
+                .padding(top = 4.dp, bottom = 4.dp, start = 8.dp, end = 8.dp)
+        ) {
+            Text(
+                text = "Action",
+                fontFamily = fontFamily,
+                fontSize = 12.sp,
+                color = BoxTextColor
+            )
+        }
+        Box(
+            modifier = Modifier
+                .border(1.dp, Color.DarkGray, RoundedCornerShape(4.dp))
+                .clip(RoundedCornerShape(4.dp))
+                .wrapContentSize()
+                .background(BoxBackground)
+                .padding(top = 4.dp, bottom = 4.dp, start = 8.dp, end = 8.dp)
+        ) {
+            Text(
+                text = "Sci-Fi",
+                fontFamily = fontFamily,
+                fontSize = 12.sp,
+                color = BoxTextColor
+            )
+        }
+    }
+    Spacer(modifier = Modifier.height(15.dp))
+    Divider(
+        color = Color.DarkGray,
+        thickness = 1.dp,
+        modifier = Modifier.padding(horizontal = 24.dp)
+    )
+}
+
+@Composable
+fun DescriptionSection(fontFamily: FontFamily, description: String) {
+    Text(
+        text = "Synopsis",
+        fontFamily = fontFamily,
+        fontSize = 16.sp,
+        color = Color.White,
+        modifier = Modifier
+            .padding(start = 24.dp, top = 16.dp, bottom = 12.dp)
+    )
+    var expandedText by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .padding(start = 24.dp, end = 24.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        if (expandedText) {
+            Text(
+                text = description,
+                fontFamily = fontFamily,
+                fontSize = 12.sp,
+                color = TextColorDetail
+            )
+            Text(
+                text = "Read less..",
+                fontFamily = fontFamily,
+                fontSize = 14.sp,
+                color = Color.White,
+                modifier = Modifier
+                    .clickable { expandedText = false }
+            )
+        } else {
+            Text(
+                text = if (description.length > 185) {
+                    description.substring(0, 185)
+                } else {
+                    description
+                },
+                fontFamily = fontFamily,
+                fontSize = 12.sp,
+                color = TextColorDetail
+            )
+            Text(
+                text = "Read more..",
+                fontFamily = fontFamily,
+                fontSize = 14.sp,
+                color = Color.White,
+                modifier = Modifier
+                    .clickable { expandedText = true }
+            )
+        }
+    }
 }
